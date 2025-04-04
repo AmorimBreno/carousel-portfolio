66796b44b94a9648a892de1181b35ca4eb9d5b00
Breno Amorim <brenoamorim11@gmail.com>
2025-04-04 09:17:14 -0300

    carrousel working

--- src/app/components/Carrousel.tsx
+++ src/app/components/Carrousel.tsx
@@ -1,20 +1,28 @@
-import { motion } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { useRef } from 'react'
 
 export default function Carrousel() {
   const slides = ['bg-red-500', 'bg-blue-500', 'bg-yellow-500', 'bg-green-500']
 
+  const targetRef = useRef<HTMLDivElement | null>(null)
+  const { scrollYProgress } = useScroll({
+    target: targetRef
+  })
+
+  const x = useTransform(scrollYProgress, [0, 1], ['0%', '-25%'])
+
   return (
-    <div className="relative ml-12 h-[400vh]">
-      <motion.div className="sticky top-0 flex h-screen items-center justify-start overflow-hidden p-2">
-        <div className="flex gap-2">
+    <section ref={targetRef} className="relative ml-12 h-[200vh]">
+      <div className="sticky top-0 flex h-screen items-center justify-start overflow-hidden p-2">
+        <motion.div className="flex gap-2" style={{ x }}>
           {slides.map((i) => (
             <div
               key={i}
-              className="h-[98vh] w-[480px] rounded-sm bg-gray-300"
+              className="h-[98vh] w-[31.6vw] rounded-sm bg-gray-300"
             />
           ))}
-        </div>
-      </motion.div>
-    </div>
+        </motion.div>
+      </div>
+    </section>
   )
 }
